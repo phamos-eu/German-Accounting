31a46b0b35e4fe1d09e3b31ef61aa71de12bb528
Muhammad Ali <swe.mirza.ali@gmail.com>
2024-03-29 15:45:25 +0000

    Removed Override Functionality on tax_category in address

--- german_accounting/events/address.py
+++
@@ -1,9 +0,0 @@
-import frappe
-from frappe.utils import flt
-
-
-def set_tax_category(doc, method=None):
-    if doc.country:
-        tax_category = frappe.get_cached_value('Country', doc.country, 'tax_category')
-        if tax_category:
-            doc.tax_category = tax_category
