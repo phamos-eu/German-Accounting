3cf76eb73739706bf4544fad4abd9cdba91f7c86
Robera16 <roberaworknehrobwork@gmail.com>
2024-04-23 10:43:33 +0300

    feat: add custom import  button

--- german_accounting/german_accounting/doctype/datev_opos_import/datev_opos_import.js
+++ german_accounting/german_accounting/doctype/datev_opos_import/datev_opos_import.js
@@ -2,7 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('DATEV OPOS Import', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (frm.doc.import_file) {
+			frm.add_custom_button(__("Start Import"), () => {
+				// frm.call("start_import", {
+				// 	file_url: frm.doc.import_file,
+				// 	bank_account: frm.doc.bank_account,
+				// 	company: frm.doc.company,
+				// });
+			});
+		}
+	}
 });
