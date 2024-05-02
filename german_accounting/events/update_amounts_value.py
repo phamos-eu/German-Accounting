fcec593913016862c458e01f0cb6020d61fed067
Robera16 <roberaworknehrobwork@gmail.com>
2024-05-02 10:55:04 +0300

    feat: add non_invoiced_amount

--- german_accounting/events/update_amounts_value.py
+++ german_accounting/events/update_amounts_value.py
@@ -45,25 +45,47 @@ def update_overdue_invoice_amount(doc):
 
 def update_non_invoiced_amount(doc):
     pass
-    # customer = doc.name
-    # total_non_invoiced_amount = 0.0
+    customer = doc.name
+    total_non_invoiced_amount = 0.0
+    total_billed_amount = 0.0
+    net_non_invoice_amount = 0.0
 
-    # sql_query = """
-    #     SELECT SUM(grand_total)
-    #     FROM `tabSales Order`
-    #     WHERE customer = %(customer)s
-    #     AND status NOT IN ('Cancelled', 'Completed','On Hold', 'Closed')
-    #     AND per_billed != '100'
-    # """
+    sql_query = """
+        SELECT SUM(base_grand_total)
+        FROM `tabSales Order`
+        WHERE customer = %(customer)s
+        AND status NOT IN ('Cancelled', 'Completed','On Hold', 'Closed')
+        AND per_billed != '100'
+    """
         
-    # result = frappe.db.sql(sql_query, {"customer": customer})
+    result = frappe.db.sql(sql_query, {"customer": customer})
     
-    # if result:
-    #     noninvoiced_amount = result[0][0]
-    #     if noninvoiced_amount:
-    #         total_non_invoiced_amount = noninvoiced_amount
+    if result:
+        noninvoiced_amount = result[0][0]
+        if noninvoiced_amount:
+            total_non_invoiced_amount = noninvoiced_amount
 
-    # doc.non_invoiced_amount = total_non_invoiced_amount
+    sql_query2 = """
+        SELECT SUM(billed_amt)
+	    FROM `tabSales Order Item`
+	    WHERE parent IN (
+	        SELECT name
+	        FROM `tabSales Order`
+	        WHERE customer = %(customer)s
+	        AND status NOT IN ('Cancelled', 'Completed', 'On Hold', 'Closed')
+	        AND per_billed != '100'
+	    )
+    """
+    result2 = frappe.db.sql(sql_query2, {"customer": customer})
+    
+    if result2:
+        billed_amount = result2[0][0]
+        if billed_amount:
+            total_billed_amount = billed_amount
+
+    net_non_invoice_amount = total_non_invoiced_amount - total_billed_amount
+
+    doc.non_invoiced_amount = net_non_invoice_amount
 
 def update_total(doc):
     pass
