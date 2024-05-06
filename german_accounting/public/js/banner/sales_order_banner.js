8bbb435070c3afbaf3e24e451025cf2cd5f8bbaa
Robera16 <roberaworknehrobwork@gmail.com>
2024-05-06 13:05:01 +0300

    feat: add currency symbol

--- german_accounting/public/js/banner/sales_order_banner.js
+++ german_accounting/public/js/banner/sales_order_banner.js
@@ -5,10 +5,11 @@ frappe.ui.form.on('Sales Order', {
 			frm.trigger('make_dashboard');
 		}
 	},
-	make_dashboard: (frm) => {
+	make_dashboard: async (frm) => {
 		if(!frm.is_new()) {
             if (frm.doc.customer) {
-                
+                const currencySymbol = await getDefaultCurrencySymbol();
+
 				let customer = frm.doc.customer
                 let open_invoice_amount = frm.doc.open_invoice_amount.toFixed(2)
                 let overdue_invoice_amount = frm.doc.overdue_invoice_amount.toFixed(2)
@@ -20,7 +21,7 @@ frappe.ui.form.on('Sales Order', {
                         <div class="col-xs-12">
                             <span class="indicator whitespace-nowrap">
                                 <span class="hidden-xs">
-                                    <p>Customer <b>${customer}</b> has an Open Invoice Amount of: <b>${open_invoice_amount}</b>, Overdue Invoice Amount of: <b>${overdue_invoice_amount}</b>, Non-Invoiced Amount of: <b>${non_invoiced_amount}</b>. This is a Total of: <b>${total}</b></p>
+                                    <p>Customer <b>${customer}</b> has an Open Invoice Amount of: <b>${currencySymbol} ${open_invoice_amount}</b>, Overdue Invoice Amount of: <b>${currencySymbol} ${overdue_invoice_amount}</b> and Non-Invoiced Amount of: <b>${currencySymbol} ${non_invoiced_amount}</b>.<br> This is a Total of: <b>${currencySymbol} ${total}</b>.</p>
                                 </span>
                             </span>
                         </div>
@@ -30,3 +31,24 @@ frappe.ui.form.on('Sales Order', {
 		}
 	}
 })
+
+
+async function getDefaultCurrencySymbol() {
+    try {
+
+        const response = await fetch("/api/resource/Global Defaults/Global Defaults");
+        const data = await response.json();
+
+        const currencyCode = data.data.default_currency;
+
+        const currencyResponse = await fetch(`/api/resource/Currency/${currencyCode}`);
+        const currencyData = await currencyResponse.json();
+
+        const currencySymbol = currencyData.data.symbol;
+
+        return currencySymbol;
+    } catch (error) {
+        console.error("Error fetching default currency symbol:", error);
+        return null;
+    }
+}
