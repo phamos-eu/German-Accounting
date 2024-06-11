669bf8b2dab1d903220fb4c255c1e383ee2197a2
Robera16 <roberaworknehrobwork@gmail.com>
2024-06-11 16:48:50 +0300

    fix: datev opos import cleanup

--- german_accounting/german_accounting/doctype/datev_opos_import/datev_opos_import.js
+++ german_accounting/german_accounting/doctype/datev_opos_import/datev_opos_import.js
@@ -1,14 +1,65 @@
 // Copyright (c) 2024, phamos.eu and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('DATEV OPOS Import', {
-	refresh: function(frm) {
-		if (frm.doc.import_file) {
-			frm.add_custom_button(__("Start Import"), () => {
-				frm.call("start_import", {
-					file_url: frm.doc.import_file,
+frappe.ui.form.on("DATEV OPOS Import", {
+	setup(frm){
+		frappe.realtime.on('update_import_status', function(data) {
+			frm.reload_doc();
+		});
+	},
+	refresh(frm) {
+		$('.menu-btn-group').remove()
+		frm.page.hide_icon_group();
+
+
+		if (frm.doc.import_file &&
+			frm.doc.status !== 'Success' && 
+            frm.doc.status !== 'Partial Success' && 
+            frm.doc.status !== 'Error'
+		) {
+			frm.disable_save();
+			frm.page.set_primary_action("Start Import", () =>  {
+					frm.call("start_import", {
+						file_url: frm.doc.import_file,
+					});
 				});
-			});
 		}
+
+
+		if (frm.doc.status.includes("Success")) {
+			frm.disable_save();
+			frm.add_custom_button(__("Go to Sales Invoice List"), () =>
+				frappe.set_route("List", "Sales Invoice")
+			);
+		}
+
+		if (frm.doc.status.includes("Error")) {
+			frm.disable_save();
+			frm.page.set_primary_action("Retry", () =>  {
+					frm.call("start_import", {
+						file_url: frm.doc.import_file,
+					});
+				});
+		}
+
+		if(!frm.is_new()){
+			frm.trigger('set_headline')
+		}
+	},
+
+	set_headline(frm){
+		let message='';
+		let indicator='';
+	
+		if (frm.doc.status === 'Success') {
+			message = `Successfully imported.`;
+			indicator = 'blue';
+		} 
+		else if (frm.doc.status === 'Partial Success') {
+			message = `Partially imported. Fix the error for unimported rows.`;
+			indicator = 'orange';
+		}
+		
+		frm.dashboard.set_headline(message, indicator);
 	}
 });
