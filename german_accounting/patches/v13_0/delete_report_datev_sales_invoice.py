3e839bd76337f01a9bcb620ef58bc05a82a721a5
Mirza Muhammad Ali Baig <swe.mirza.ali@gmail.com>
2024-07-25 20:15:02 +0000

    Add sql code in conditional statement

--- german_accounting/patches/v13_0/delete_report_datev_sales_invoice.py
+++ german_accounting/patches/v13_0/delete_report_datev_sales_invoice.py
@@ -5,10 +5,10 @@ def execute():
 	"""Check Report and delete"""
 	if frappe.db.exists("Report", "DATEV Sales Invoice"):
 		frappe.delete_doc("Report", "DATEV Sales Invoice", force=1)
-	frappe.db.sql(
+		frappe.db.sql(
+			"""
+			DELETE FROM `tabReport`
+			WHERE name = 'DATEV Sales Invoice'
 		"""
-		DELETE FROM `tabReport`
-		WHERE name = 'DATEV Sales Invoice'
-	"""
-	)
-	frappe.db.commit()
+		)
+		frappe.db.commit()
