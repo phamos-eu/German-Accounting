b36d6b913783b151ecb4e36b7a69ac1d84a3b1b6
Robera16 <roberaworknehrobwork@gmail.com>
2024-08-01 11:59:33 +0300

    fix/28-review-comments

--- german_accounting/public/js/banner/sales_order_banner.js
+++ german_accounting/public/js/banner/sales_order_banner.js
@@ -1,45 +1,42 @@
-frappe.require('/assets/german_accounting/js/banner_utils.js')
+frappe.require('/assets/german_accounting/js/banner_utils.js');
+
 frappe.ui.form.on('Sales Order', {
-    customer: async (frm) => {
-        if(frm.doc.customer) {
+    customer: (frm) => {
+        if (frm.doc.customer) {
             frm.trigger('make_dashboard');
         }
-    }, 
-	refresh: (frm) => {
-			frm.trigger('make_dashboard');
-	},
-	make_dashboard: async (frm) => {
-            if (frm.doc.customer) {
-                const customer = frm.doc.customer
-                const company = frm.doc.company
-                const doctype = frm.doc.doctype
-                const docname = frm.doc.name
+    },
+    refresh: (frm) => {
+        frm.trigger('make_dashboard');
+    },
+    before_submit: async (frm) => {
+        const { customer, company, doctype, name: docname } = frm.doc;
 
-                const data_for_headline_text = await getDataForHeadlineText(customer, company, doctype);
-                const currency_symbol = data_for_headline_text.currency_symbol;
-                const credit_limit =  data_for_headline_text.credit_limit.toFixed(2);
-                const bypass_checked =  data_for_headline_text.bypass_checked;
-                const amounts = data_for_headline_text.amounts;
-                const total = amounts.total.toFixed(2)
-                const credit_limit_text = getCreditLimitText(credit_limit, currency_symbol)          
+        const { amounts, credit_limit, check_bypass } = await getDataForHeadlineText(customer, company, doctype);
+        const total = parseFloat(amounts.total).toFixed(2);
 
-                let text_class = '';
-                if ((parseFloat(total) > parseFloat(credit_limit)) && parseFloat(credit_limit) > 0) {
-                    if (!bypass_checked){
-                        $('button[data-label="Submit"]').off()
-                        $('button[data-label="Submit"]').click(() => {checkCreditLimit(frm, customer, company, doctype, docname, total)});   
-                    }
-               
-                    text_class = 'text-danger';
-                }
+        if (parseFloat(total) > parseFloat(credit_limit) && parseFloat(credit_limit) > 0 && !check_bypass) {
+            frappe.validated = false;
+            checkCreditLimit(frm, customer, company, doctype, docname, total);
+        }
+    },
+    make_dashboard: async (frm) => {
+        const { customer, company, doctype } = frm.doc;
 
-                const headline_text = getHeadlineText(customer ,amounts, credit_limit_text,text_class, currency_symbol)
+        if (customer) {
+            const { currency_symbol, credit_limit, amounts } = await getDataForHeadlineText(customer, company, doctype);
+            const total = parseFloat(amounts.total).toFixed(2);
+            const credit_limit_text = getCreditLimitText(parseFloat(credit_limit).toFixed(2), currency_symbol);
 
-                frm.dashboard.clear_headline();
-                frm.dashboard.set_headline_alert(headline_text);
-                
+            let text_class = '';
+            if (parseFloat(total) > parseFloat(credit_limit) && parseFloat(credit_limit) > 0) {
+                text_class = 'text-danger';
             }
-	},
 
-})
+            const headline_text = getHeadlineText(customer, amounts, credit_limit_text, text_class, currency_symbol);
+            frm.dashboard.clear_headline();
+            frm.dashboard.set_headline_alert(headline_text);
+        }
+    }
+});
  
